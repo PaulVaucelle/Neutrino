5196f38014bd3dded62899cb48e0edfbaef3d2e5
paul <paul.vaucelle@etu.unistra.fr>
2022-02-15 05:53:52 -0300

    Does everything

--- RIBD.C
+++ RIBD.C
@@ -6,14 +6,15 @@
     #include <TStyle.h>
     #include "stdio.h"
     #include <TGraph.h>
+    #include <TRandom.h>
     
     
     #define m 200
     #define Nbcore 10
     /*Needed parameters*/
-
-   double Emin=2.0;/*MeV a little bit less in reality, this is an approximation*/
-    double Emax=8.21; /*MeV as the energy of neutrinos coming from NPP reaches only around 10MeV*/
+    
+    double Emin=2.0;/*MeV a little bit less in reality, this is an approximation*/
+    double Emax=10; /*MeV as the energy of neutrinos coming from NPP reaches only around 10MeV*/
     double baseline[Nbcore]={52750,52840,52420,52510,52120,52210,52760,52630,52320,52200}; /*m could be one of the future entry parameter of our code*/
     double Gth[Nbcore]={2.9e9,2.9e9,2.9e9,2.9e9,2.9e9,2.9e9,4.6e9,4.6e9,4.6e9,4.6e9};
     double Mbaseline=(baseline[0]+baseline[1]+baseline[2]+baseline[3]+baseline[4]+baseline[5]+baseline[6]+baseline[7]+baseline[8]+baseline[9])/10;
@@ -115,8 +116,34 @@
     double Na = 6.022e23;
     double nbrC = nc*Na;
     double nbrH = nh*Na;
-    double Np = nbrH+6*nbrC;
-    /*-------------------------------------*/
+    double Np = nbrH;
+    /*-------------Modif 12/02/2022------------------------*/
+
+    TCanvas *c10 = new TCanvas("c10","Comparison",200,10,600,400);
+    /*Cross section*/
+    TF1 *Xsection = new TF1("Xsection","0.0952e-42*sqrt((x-[0])*(x-[0])-[1]*[1])*(x-[0])",Emin, Emax);
+    Xsection->SetParameter(0,D);
+    Xsection->SetParameter(1,m_e);
+    //check the parameters of [0]
+    /*Neutrino flux for each isotopes wieghted by their fission fraction*/
+    //func () {} ==> replace string bettre efficiency don't need ti take mean [h], take ei +ei+1/2
+    TF1 *U235NFlux = new TF1("U235NFlux","[0]*exp(0.870-0.160*x-0.091*x*x)",Emin,Emax);
+    U235NFlux->SetParameter(0,fi235U);
+    
+    TF1 *Pu239NFlux = new TF1("Pu239NFlux","[0]*exp(0.896-0.239*x-0.0981*x*x)",Emin,Emax);
+    Pu239NFlux->SetParameter(0,fi239Pu);
+
+    TF1 *U238NFlux = new TF1("U238NFlux","[0]*exp(0.976-0.162*x-0.0790*x*x)",Emin,Emax);
+    U238NFlux->SetParameter(0,fi238U);
+
+    TF1 *Pu241NFlux = new TF1("Pu241NFlux","[0]*exp(0.793-0.080*x-0.1085*x*x)",Emin,Emax);
+    Pu241NFlux->SetParameter(0,fi241Pu);
+
+    TF1 *TotalNFLux = new TF1("TotalNFLux","(U235NFlux+Pu239NFlux+U238NFlux+Pu241NFlux)*Xsection",Emin,Emax);
+    /* the flux are the one being differents....Xsection seems good*/
+    
+    /*------------------------------------------------*/
+
     TF1 *fa3 = new TF1("fa3","[0]*exp(0.870-0.160*x-0.091*x*x)*0.0952e-42*sqrt((x-[5])*(x-[5])-[6]*[6])*(x-[5])+[1]*exp(0.896-0.239*x-0.0981*x*x)*0.0952e-42*sqrt((x-[5])*(x-[5])-[6]*[6])*(x-[5])+[3]*exp(0.976-0.162*x-0.0790*x*x)*0.0952e-42*sqrt((x-[5])*(x-[5])-[6]*[6])*(x-[5])+[4]*exp(0.793-0.080*x-0.1085*x*x)*0.0952e-42*sqrt((x-[5])*(x-[5])-[6]*[6])*(x-[5])",Emin,Emax);
     /*TF1 *fa3 = new TF1("fa3","fi235U[0]*exp(0.870-0.160*E-0.091*E*E)*0.0952e-42*sqrt((E-D)*(E-D)-m_e*m_e)*(E-D)+fi239[1]Pu*exp(0.896-0.239*E-0.0981*E*E)*0.0952e-42*sqrt((E-D)*(E-D)-m_e*m_e)*(E-D)+fi238U[3]*exp(0.976-0.162*E-0.0790*E*E)*0.0952e-42*sqrt((E-D)*(E-D)-m_e*m_e)*(E-D)+fi241Pu[4]*exp(0.793-0.080*E-0.1085*E*E)*0.0952e-42*sqrt((E-D)*(E-D)-m_e*m_e)*(E-D)",Emin,Emax);*/
     fa3->SetParameter(0,fi235U);
@@ -128,9 +155,19 @@
     // fa3->SetTitle("function to integrate wrt the neutrino energy");
     // fa3->GetXaxis()->SetTitle("Energy of the Neutrino (MeV)");
     // fa3->GetYaxis()->SetTitle("Integrated function");
-    // fa3->Draw();
+    TotalNFLux->SetLineColor(3);/*totalnflux*/
+    TotalNFLux->Draw();
+    fa3->SetLineColor(4);
+    fa3->Draw("L");// 
+    auto legend7 = new TLegend(0.9,0.75,0.7,0.9);/*(gap from left,Gap from the bottom,percentage width filled,gap fro mtop?)*/
+    legend7->SetHeader("Legend","C"); /*C to center the title*/
+    legend7->AddEntry(fa3,"fa3");
+    legend7->AddEntry(TotalNFLux,"TotalNFLux");
+    legend7->Draw();/* ACTIVATE/DESACVTIVATE*/
+
 
     /*Proba computation*/
+    
     E[0]=Emin;
     TH1F *hist = new TH1F("hist", "Histogram",m,2.00,8.21);
     // TCanvas *c5 = new TCanvas();
@@ -179,16 +216,17 @@
             F12[i]=X[i]*Phi[i]*(1-P21[i]);/*No Mass Ordering part*/
             IF321[i]=X[i]*Phi[i]*IP321[i];/* Inverted Hierarchy*/
             
-            F321res[i]=F321[i]*ROOT::Math::gaussian_pdf(E[i],0.03*sqrt(E[i]),E[i+1]/2+E[i]/2);
+            F321res[i]=F321[i]*ROOT::Math::gaussian_pdf(E[i],0.03/sqrt(E[i]),E[i+1]/2+E[i]/2);
             hist->Fill(E[i],F321[i]);
             ey[i]=0;
         }
+    //X[0]=0;
     /*hist->Draw("HIST SAME C");*/
     // double scale= 1/hist->Integral();
     // hist->GetXaxis()->SetTitle("Neutrino Energy MeV");
     // hist->GetYaxis()->SetTitle("Neutrino Spectra");
     // hist->SetFillColor(kBlue-7);
-    // hist->Draw("H");
+    //hist->Draw("H");
  
     /*TGraph part*/
 
@@ -203,7 +241,7 @@
             
         }
     TH1F *hist2 = new TH1F("hist2", "Histogram",m,1.00,10.00);
-    TCanvas *c6 = new TCanvas();
+    //TCanvas *c6 = new TCanvas();
         for (int b=0;b<m;b++)
         /*sums over bins*/         
             {for (int r=0;r<Nbcore;r++)
@@ -213,23 +251,61 @@
                 Const[r]= Gth[r]*1/(Sumfiei)*TimeConverter*6.42*pow(10,+12)/(4*4*atan(1.0)*baseline[r]*baseline[r]*(1e4))*epsilon*Np;/*Units correction L in cm*/
 
                 /*printf("Constr = %f \n", Const[r]);*/
-                SiX[b]= fa3->Integral(E[b],E[b+1],1e-12);
-
+                SiX[b]= fa3->Integral(E[b],E[b+1],1e-12);/*fa3->*/
                 /*printf("E[b] = %f  E[b+1] = %f \n",E[b],E[b+1]);*/
                 ItotE[b]=Const[r]*SiX[b]*P321[b];
                 Itot=Itot+Const[r]*SiX[b]*P321[b];/* sum all over the bins*//*13.5*//*Doesn't take into accout the oscillations*/
                 hist2->Fill(Evis[b],ItotE[b]);
                 }
             }
+            ItotE[m-1]=0;
      /*hist->Draw("HIST SAME C");*/
     double scale= 1/hist2->Integral();
     hist2->GetXaxis()->SetTitle("Neutrion visible energy");
     hist2->GetYaxis()->SetTitle("IBD events per day");
     hist2->SetFillColor(kBlue-7);
-    hist2->Draw("H");
+    //hist2->Draw("H");
+
+
+
+    TH1F *hist3 = new TH1F("hist3", "Energy resolution",m,2.00,8.21);
+    //Energy resolution
+    double Nevent[m];
+    double SumGaus[m];
+    double mean;
+    double gausnum;
+    TRandom *Uni = new TRandom();
+    
+    for(int s=0;s<m;s++)//For each bin
+        {
+            Nevent[s]=ItotE[s]*5000;//we get the number of events per bin
+            for(int h=0;h<Nevent[s];h++)//for each bin with a certain number of event, we generate an event
+                {
+                    mean= Uni->Uniform(E[s],E[s+1]);/*get a random number in the bin that is gonna be the mean for the gaussian*/
+                    gausnum = Uni->Gaus(mean,0.03*sqrt(mean));
+                    hist3->Fill(gausnum);
+                    // for (int w=0;w<m;w++)//for each bin, i have to add the value of the distribution(gaussian) of the considered event
+                    //     {
+                    //         SumGaus[w]=(SumGaus[w]+ItotE[w]*ROOT::Math::gaussian_pdf(E[w],0.03/sqrt(mean),mean));//Not sure about the weighting factor
+                    //     }
+                }
+        }
+    hist3->Draw();
+
+    TCanvas *c7 = new TCanvas("c7","Energy resolution test",200,10,600,400);
+    TGraph *gr11 = new TGraph(m,E,SumGaus);
+    gr11->SetLineColor(6);
+    gr11->SetTitle("Spectra with Eres");
+    gr11->GetXaxis()->SetTitle("Energy of the Neutrino (MeV)");
+    gr11->GetYaxis()->SetTitle("Neutrino spectra");
+    gr11->Draw("AL");
+
+
+    /*ENd of energy resolution part*/
+
 
     TCanvas *c3 = new TCanvas("c3","Ibd events per day as a function of energy",200,10,600,400);
-    TGraph *gr6 = new TGraph(m,Evis,ItotE);/*Si on trace I, on retrouve la forme caluler pour F1 dans le code RFluxocs.C*/
+    TGraph *gr6 = new TGraph(m,E,ItotE);/*Si on trace I, on retrouve la forme caluler pour F1 dans le code RFluxocs.C*/
     gr6->SetLineColor(6);
     gr6->SetTitle("IBD events per day as a function of the energy");
     gr6->GetXaxis()->SetTitle("Energy of the Neutrino (MeV)");
@@ -290,8 +366,8 @@
     legend->Draw();/* ACTIVATE/DESACVTIVATE*/
 
     TCanvas *c4 = new TCanvas("c4","Fourth Graph",200,10,600,400);
-    auto gr = new TGraph(m,Evis,F321res);/*F321*//*use small bin number defined by the constaint of nergy resolution*/
-    TGraph *gr9 = new TGraph(m,Evis,F321);/*IF321*/
+    auto gr = new TGraph(m,Evis,F321);/*Evis F321*//*use small bin number defined by the constaint of nergy resolution*/
+    TGraph *gr9 = new TGraph(m,Evis,IF321);/*Evis IF321*/
     gr->SetTitle("Observed Neutrino Spectrum with oscillations");
     gr->GetXaxis()->SetTitle("Visible Energy(MeV)");
     gr->GetYaxis()->SetTitle("Observed Neutrino Spectrum with oscillations");
